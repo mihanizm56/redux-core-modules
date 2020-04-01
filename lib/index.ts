f4363888027046c404618438e3e8776b2f36ae1a
mihanizm56 <m727507.56@mail.ru>
2020-04-02 02:26:31 +0300

    test(configure-package): upd

--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,37 @@
-export {createAppStore} from './store'
+export { createAppStore } from './store';
 
-export {Action,BaseAction} from './types'
+export { Action, BaseAction, IAdvancedStore } from './types';
 
-export {injectAsyncReducer} from './utils/inject-reducers'
-export {injectAsyncSaga} from './utils/inject-sagas'
+export { injectAsyncReducer } from './utils/inject-reducers';
+export { injectAsyncSaga } from './utils/inject-sagas';
 
-export {fetchFormManagerSagaAction, FormManagerType} from './root-modules/form-manager-module'
-export {initLoadManagerActionSaga, InitLoadManagerSourceType, InitLoadManagerActionPayloadType} from './root-modules/init-load-manager-module'
-export {requestExtraDataHandlerActionSaga} from './root-modules/request-extra-data-handler-module'
-export {NotificationType, setModalAction, getModalStack, removeModalAction} from './root-modules/notifications-module'
-export {IRedirectManagerPayload} from './root-modules/redirect-manager-module'
-export {InitRequestHandlerActionType} from './root-modules/request-extra-data-handler-module'
-export {getPageIsLoading, getIsAppError} from './root-modules/ui-module'
+export {
+  fetchFormManagerSagaAction,
+  FormManagerType,
+} from './root-modules/form-manager-module';
 
+export {
+  initLoadManagerActionSaga,
+  InitLoadManagerSourceType,
+  InitLoadManagerActionPayloadType,
+} from './root-modules/init-load-manager-module';
+
+export {
+  requestExtraDataHandlerActionSaga,
+} from './root-modules/request-extra-data-handler-module';
+
+export {
+  NotificationType,
+  setModalAction,
+  getModalStack,
+  removeModalAction,
+} from './root-modules/notifications-module';
+
+export {
+  IRedirectManagerPayload,
+} from './root-modules/redirect-manager-module';
+
+export {
+  InitRequestHandlerActionType,
+} from './root-modules/request-extra-data-handler-module';
+export { getPageIsLoading, getIsAppError } from './root-modules/ui-module';
