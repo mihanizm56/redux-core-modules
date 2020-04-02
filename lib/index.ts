bb93583f06f6fb02c719376d2a8f126dccf2ed20
mihanizm56 <m727507.56@mail.ru>
2020-04-02 03:29:49 +0300

    feat(scripts): added tese default export

--- lib/index.ts
+++ lib/index.ts
@@ -8,12 +8,14 @@ export { injectAsyncSaga } from './utils/inject-sagas';
 export {
   fetchFormManagerSagaAction,
   FormManagerType,
+  formManagerWatcherSaga,
 } from './root-modules/form-manager-module';
 
 export {
   initLoadManagerActionSaga,
   InitLoadManagerSourceType,
   InitLoadManagerActionPayloadType,
+  initLoadManagerWatcherSaga,
 } from './root-modules/init-load-manager-module';
 
 export {
@@ -29,9 +31,14 @@ export {
 
 export {
   IRedirectManagerPayload,
+  redirectManagerWatcherSaga,
 } from './root-modules/redirect-manager-module';
 
 export {
   InitRequestHandlerActionType,
+  requestExtraDataHandlerWatcherSaga,
 } from './root-modules/request-extra-data-handler-module';
+
 export { getPageIsLoading, getIsAppError } from './root-modules/ui-module';
+
+export { default as uiModuleReducer } from './root-modules/ui-module';
