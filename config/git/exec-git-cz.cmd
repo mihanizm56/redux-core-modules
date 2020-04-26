3a2bc57b1a503ebbd84ac43746098ecc2c2cc3ae
mihanizm56 <m727507.56@mail.ru>
2020-04-27 02:03:20 +0300

    fix commitizen

--- config/git/exec-git-cz.cmd
+++ config/git/exec-git-cz.cmd
@@ -1,19 +1,19 @@
-@ECHO off
-SETLOCAL
+    @ECHO off
+    SETLOCAL
 
-CALL :find_dp0
-set "_prog=%dp0%node_modules\.bin\git-cz.cmd"
-IF NOT EXIST "%_prog%" (
-    ECHO "%_prog% does not exist, please install commitizen"
+    CALL :find_dp0
+    set "_prog=%dp0%node_modules\.bin\git-cz.cmd"
+    IF NOT EXIST "%_prog%" (
+        ECHO "%_prog% does not exist, please install commitizen"
+        ENDLOCAL
+        EXIT /b 1
+    )
+
+    call "%_prog%" %*
+
     ENDLOCAL
-    EXIT /b 1
-)
+    EXIT %errorlevel%
 
-call "%_prog%" %*
-
-ENDLOCAL
-EXIT %errorlevel%
-
-:find_dp0
-SET dp0=%~dp0
-EXIT /b
+    :find_dp0
+    SET dp0=%~dp0
+    EXIT /b
