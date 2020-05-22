3b8967801aa49c74043dbdc082d0a21339928779
mihanizm56 <m727507.56@mail.ru>
2020-05-23 01:55:37 +0300

    build(configure-project): speed up combine reducers

--- lib/utils/combine-reducers.ts
+++ lib/utils/combine-reducers.ts
@@ -5,19 +5,6 @@ import {
   Action,
 } from 'redux';
 
-const randomString = () =>
-  Math.random()
-    .toString(36)
-    .substring(7)
-    .split('')
-    .join('.');
-
-const ActionTypes = {
-  INIT: `@@redux/INIT${/* #__PURE__ */ randomString()}`,
-  REPLACE: `@@redux/REPLACE${/* #__PURE__ */ randomString()}`,
-  PROBE_UNKNOWN_ACTION: () => `@@redux/PROBE_UNKNOWN_ACTION${randomString()}`,
-};
-
 function getUndefinedStateErrorMessage(key: string, action: Action) {
   const actionType = action && action.type;
   const actionDescription =
@@ -28,38 +15,6 @@ function getUndefinedStateErrorMessage(key: string, action: Action) {
     'To ignore an action, you must explicitly return the previous state. ' +
     'If you want this reducer to hold no value, you can return null instead of undefined.'
   );
-}
-
-function assertReducerShape(reducers: ReducersMapObject) {
-  Object.keys(reducers).forEach(key => {
-    const reducer = reducers[key];
-    const initialState = reducer(undefined, { type: ActionTypes.INIT });
-
-    if (typeof initialState === 'undefined') {
-      throw new Error(
-        `Reducer "${key}" returned undefined during initialization. ` +
-        'If the state passed to the reducer is undefined, you must ' +
-        'explicitly return the initial state. The initial state may ' +
-          'not be undefined. If you don\'t want to set a value for this reducer, ' + // eslint-disable-line
-          'you can use null instead of undefined.',
-      );
-    }
-
-    if (
-      typeof reducer(undefined, {
-        type: ActionTypes.PROBE_UNKNOWN_ACTION(),
-      }) === 'undefined'
-    ) {
-      throw new Error(
-        `Reducer "${key}" returned undefined when probed with a random type. ` +
-          `Don't try to handle ${ActionTypes.INIT} or other actions in "redux/*" ` +
-          'namespace. They are considered private. Instead, you must return the ' +
-          'current state for any unknown actions, unless it is undefined, ' +
-          'in which case you must return the initial state, regardless of the ' +
-          'action type. The initial state may not be undefined, but can be null.',
-      );
-    }
-  });
 }
 
 // default combineReducers from redux but without warning about reducers keys !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -82,21 +37,10 @@ export function combineReducers(reducers: ReducersMapObject) {
   }
   const finalReducerKeys = Object.keys(finalReducers);
 
-  let shapeAssertionError: Error;
-  try {
-    assertReducerShape(finalReducers);
-  } catch (e) {
-    shapeAssertionError = e;
-  }
-
   return function combination(
     state: StateFromReducersMapObject<typeof reducers> = {},
     action: AnyAction,
   ) {
-    if (shapeAssertionError) {
-      throw shapeAssertionError;
-    }
-
     let hasChanged = false;
     const nextState: StateFromReducersMapObject<typeof reducers> = {};
     // eslint-disable-next-line
